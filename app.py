bfad4f7cdf8c21801cf2d419c8c41ed99810eadc
jacobocastros31 <jacobocastros31@gmail.com>
2025-09-04 13:34:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from gtts import gTTS
 from PIL import Image
 import base64
 
-st.title("Conversión de Texto a Audio")
+st.title("Conversión de Texto a Audio Jacobo")
 image = Image.open('gato_raton.png')
 st.image(image, width=350)
 with st.sidebar:
